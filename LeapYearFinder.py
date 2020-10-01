00c7375b6078307a3fe91cf011c2fea795cddb14
groofee <72187808+groofee@users.noreply.github.com>
2020-10-01 12:39:25 +0530

    Update LeapYearFinder.py

--- LeapYearFinder.py
+++ LeapYearFinder.py
@@ -4,49 +4,28 @@ else it prints "It is not a leap year".The program ends only of the user wants i
 
 """
 
-import sys 
-import time
+# --- Leap Year Finder --- #
+while True:
+    try:
+        year = int(input("Enter a year: "))
+    except ValueError:
+        print("Sorry, that is not a valid year. Please try again.")
+        continue
+    else:
+        break
     
-print "\n ----------------Welcome to the Leap Year Finder !!!!! ------------------"
-rep=0
-while(rep!=1):
-    print "\n"
-    tryuntil=0
-    while(tryuntil!=1):
-        try:
-            inp=int(raw_input("Please enter a year :"))
-            if (inp%4==0):
-                if(inp%100==0):
-                    if (inp%400==0):
-                        print"\n This is a leap year"
-                    else:
-                        print"\n This is not a leap year"
-                else:
-                    print "\n This is a leap year"
+if (year < 1582):
+    print("Sorry, {} pre-dates the Gregorian calendar.".format(year))
+
+# --- The Triangle of Doom --- #  
+else:
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print("{} is a leap year!".format(year))
             else:
-                print"\n This is not a leap year"
-            
-            tryuntil=1
-                
-        except:
-            print "\nPlease Enter a numeric integer value"
-    
-    print "\n"
-    
-    checkoption=0
-    while(checkoption!=1):
-        try:
-            print"\n"
-            ask=raw_input("Do you want to try it again ??? y or n")
-            if (ask=='y' or ask=='Y'):
-                checkoption=1
-                continue
-            elif (ask=='n' or ask=='N'):
-                checkoption=1
-                rep=1
-                print"\n Thank you for using the leap year checker"
-            else:
-                raise NameError
-        except:
-            print"\n"
-            print "Please enter a valid option. press 'y' for Yes and 'n' for No "
+                print("{} is a common year!".format(year))
+        else:
+            print("{} is a leap year!".format(year))
+    else:
+        print("{} is a common year!".format(year))
